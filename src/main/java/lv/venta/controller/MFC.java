607a876ab1082fb09a21e5724d19ece56336d3f7
Davids-Adamovics <davidsadamovicss@gmail.com>
2025-02-15 09:54:38 +0200

    Edit

---
+++ src/main/java/lv/venta/controller/MFC.java
@@ -0,0 +1,30 @@
+package lv.venta.controller;
+
+import java.util.Random;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import ch.qos.logback.core.model.Model;
+
+@Controller
+public class MFC {
+
+		@GetMapping("/simple")
+		public String getControllerSimple() 
+		{
+			System.out.println("ir izsaukusies simple funkcija");
+			return "simple_page";
+		}
+		@GetMapping("/simple2")
+		public String getControllerSimple2(Model model) 
+		{
+			System.out.println("ir izsaukusies simple funkcija");
+			Random rand = new Random();
+			String data = "Kristers: " + rand.nextInt(0,100);
+			model.addAttribute("package", data);
+			return "simple_page2";
+		}
+		
+		
+}
