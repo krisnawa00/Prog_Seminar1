226cbd215044f0e93a4c994861ede6094255c7fb
krisnawa00 <kristersrudzitiss@gmail.com>
2025-02-28 16:57:26 +0200

    jaunumi!

---
+++ src/main/java/lv/venta/service/ICRUDProduct.java
@@ -0,0 +1,29 @@
+package lv.venta.service;
+
+import java.util.ArrayList;
+
+import lv.venta.model.Product;
+
+public interface ICRUDProduct {
+	
+	
+	//C
+	public abstract void create(String title, String desc, float cena,int quantity);
+	
+	//R
+	
+	public abstract ArrayList<Product> retrieveAll() throws Exception;
+	
+	//R
+	
+	public abstract Product retrieveById(int id) throws Exception;
+	
+	//U - Update
+	
+	public abstract void updateById (int id, float price, int quantity) throws Exception;
+	
+	//D - Delete
+	
+	public abstract void deleteById(int id) throws Exception;
+
+}
